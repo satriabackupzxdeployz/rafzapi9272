2b089dfabda2c92be4b6dbc00633f0c9cb94b8dd
satriabackupzxdeployz <satriabackupz@gmail.com>
2026-06-21 22:04:35 +0700

    Add app/dashboard/page.js

---
+++ app/dashboard/page.js
@@ -0,0 +1,9 @@
+import DashboardView from 'components/DashboardView'
+
+export const metadata = {
+  title: 'Dashboard'
+}
+
+export default function DashboardPage() {
+  return <DashboardView />
+}
